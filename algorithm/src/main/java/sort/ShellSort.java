f7d13f1da8fce82123069baaca4fd8127d146cf6
chenjiancong <jiancong.chen@Tigerobo.com>
2019-10-10 16:04:49 +0800

    希尔排序

--- algorithm/src/main/java/sort/ShellSort.java
+++ algorithm/src/main/java/sort/ShellSort.java
@@ -4,6 +4,36 @@ package sort;
  * @author : jiancongchen on 2019/10/9
  */
 
-public class ShellSort {
+public class ShellSort implements Sort{
 
+    @Override
+    public void sort(int array[]){
+        int temp = 0;
+        int incre = array.length;
+
+        while(true){
+            incre = incre/2;
+            //根据增量分为若干子序列
+            for(int k = 0; k < incre; k++){
+                for(int i = k + incre; i < array.length; i += incre){
+                    for(int j = i; j > k; j -= incre){
+                        if(array[j] < array[j-incre]){
+                            temp = array[j-incre];
+                            array[j-incre] = array[j];
+                            array[j] = temp;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+            }
+            for(int index : array){
+                System.out.print(index + " ");
+            }
+            System.out.println();
+            if(incre == 1){
+                break;
+            }
+        }
+    }
 }
